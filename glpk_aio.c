e1094e1d9e6b2b235a81bc07c87e1c4e9e5a920b
Jörg Sommrey <28217714+jo-37@users.noreply.github.com>
2024-02-13 12:07:46 +0100

    fix fix for GLP_DB

--- glpk_aio.c
+++ glpk_aio.c
@@ -121,7 +121,7 @@ glpk (int sense, int n, int m, double *c, int nz, int *rn, int *cn,
         }
 
 	  // Fix GLP_DB
-      glp_set_row_bnds (lp, i+1, typx, typx == GLP_DB ? b[i]: -b[i], b[i]);
+      glp_set_row_bnds (lp, i+1, typx, typx == GLP_DB ? -b[i]: b[i], b[i]);
 
     }
 
